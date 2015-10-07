60b8e527272445da83c7535d70f7a3d26801f14a
Jamie Alessio <jamie@stoic.net>
2015-10-07 11:16:37 -0700

    Example of hosting the zip file as a Github release.

--- Casks/yujitach-menumeters.rb
+++ Casks/yujitach-menumeters.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'yujitach-menumeters' do
-  version '1.9'
-  sha256 'ecfda83c213c5562b9be1b9fd430c78303d5b246d92e3e31822dcbe01acf3107'
+  version '1.9.1'
+  sha256 'cffa931287fe0b4f7b05e400b96bead4317702524233bafc25460687ed20aebf'
 
-  url "http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/zips/MenuMeters_#{version}.zip"
+  # url "https://github.com/yujitach/MenuMeters/releases/download/#{version}/MenuMeters_#{version}.zip"
+  url "https://github.com/jalessio/MenuMeters/releases/download/#{version}/MenuMeters_#{version}.zip"
   name 'MenuMeters'
   name 'MenuMeters El Capitan Port'
   homepage 'http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/'
